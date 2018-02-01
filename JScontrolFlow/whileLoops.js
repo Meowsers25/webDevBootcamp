03630310e64887025de40531042197846ca65322
Chris <c.lamothe25@gmail.com>
2018-02-01 16:16:45 -0500

    while loops practice

---
+++ JScontrolFlow/whileLoops.js
@@ -0,0 +1,29 @@
+var num = -10;
+
+while (num <= 19) {
+    console.log(num);
+    num++;
+};
+
+var even = 10;
+
+while (even <= 40) {
+    console.log(even);
+    even +=2;
+};
+
+var odd = 301;
+
+while (odd <= 333) {
+    console.log(odd);
+    odd +=2;
+};
+
+var divisible = 5;
+
+while (divisible <= 50) {
+    if (divisible % 5 === 0 && divisible % 3 === 0) {
+        console.log(divisible);
+    }
+    divisible++;
+};
