6517cc4aec467ce34d56696e5a7c538d7b0d8142
Chris <c.lamothe25@gmail.com>
2018-02-01 20:49:36 -0500

    working on for loops

---
+++ JScontrolFlow/forLoops.js
@@ -0,0 +1,19 @@
+for (var num = -10; num <= 19; num++) {
+    console.log(num);
+};
+
+for (var even = 10; even <= 40; even+=2) {
+    console.log(even);
+};
+
+for (var odd = 300; odd <= 333; odd++) {
+    if(odd % 2 !== 0) {
+        console.log(odd);
+    }
+};
+
+for (var i = 5; i <= 50; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+        console.log(i);
+    }
+}
