854292c950cbd97e45fc8b67f484a8cf91db4176
Chris <c.lamothe25@gmail.com>
2018-02-04 17:47:40 -0500

    scope exercises

---
+++ jsFunctions/scope.js
@@ -0,0 +1,35 @@
+var x = "Hello";
+
+function doMath() {
+    var x = 40;
+    console.log(x);
+}
+
+var y = 99;
+
+function doMoreMath() {
+    y = 100;
+    console.log(y);
+}
+
+var phrase = "Hi there!";
+
+function doSomething() {
+    var phrase = "Goodbye!";
+    console.log(phrase);
+}
+
+var num = 8;
+
+function doMathy() {
+    num += 1;
+    if(num % 5 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// num += 1;
+// doMathy();
