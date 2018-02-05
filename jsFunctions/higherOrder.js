b44551ceca015ae26b4fa9f5cc0e578941b4638d
Chris <c.lamothe25@gmail.com>
2018-02-04 22:43:00 -0500

    Higher order functions code along

---
+++ jsFunctions/higherOrder.js
@@ -0,0 +1,10 @@
+function sing() {
+    console.log("Twinkle twinkle");
+    console.log("How I wonder");
+}
+
+// setInterval(sing, 1000);
+setInterval(function(){
+    console.log("This is.....");
+    console.log("Awesome!");
+}, 2000);
